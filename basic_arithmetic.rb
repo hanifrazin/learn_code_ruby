64171f8333c35c2a24dd717f626e6c276ddb2839
Hanif Razin Rahmatullah <hanifrazin@gmail.com>
2019-06-03 13:29:03 +0700

    Basic Arithmetic

---
+++ basic_arithmetic.rb
@@ -0,0 +1,20 @@
+#plus arithmetic
+p 10+14
+
+#minus arithmetic
+p 10-14
+
+#multiply atau perkalian
+p 10*14
+
+#division
+p 10/14
+
+#pow atau pangkat
+p 10 ** 14
+
+#modulo atau sisa hasil bagi
+p 10/6
+
+#float division
+p 10.0/6
