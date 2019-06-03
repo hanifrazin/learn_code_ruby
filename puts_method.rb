dae251e0dee17ee9bc7710b488417c97f12a74c9
Hanif Razin Rahmatullah <hanifrazin@gmail.com>
2019-06-03 12:46:24 +0700

    Some example puts method in ruby

--- puts_method.rb
+++ puts_method.rb
@@ -1,2 +1,11 @@
 puts "Hello World"
 puts "I'm alive and well"
+
+puts "I bought 5 dollars at the store."
+puts "I made $65 dollars or a profit of 25%."
+
+puts 5
+puts 0.578
+
+puts 12+19
+puts "20"+"12"
